f3950898b3001230107f1991f9e2d4173ee79c80
livmas <seiranyan.sg@gmail.com>
2024-09-03 09:57:11 +0300

    Text changed

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Эфбо-01-22 Сейранян Самвел Гагикович',
             ),
             Text(
               '$_counter',
